eb848fb54159c62f1f3850d526de085f2cdc4725
ANGLAS-MECHATRONICS <vlpxnda1234@gmail.com>
2023-10-04 11:25:16 -0500

    INA226_LCD_I2C

--- STM32F103C8T6/LIBRERIAS CON HAL/INA226/Core/Startup/startup_stm32f103c8tx.s
+++ STM32F103C8T6/LIBRERIAS CON HAL/INA226/Core/Startup/startup_stm32f103c8tx.s
@@ -61,7 +61,7 @@ defined in linker script */
 Reset_Handler:
 
 /* Call the clock system initialization function.*/
-    bl  SystemInit
+  bl  SystemInit
 
 /* Copy the data segment initializers from flash to SRAM */
   ldr r0, =_sdata
